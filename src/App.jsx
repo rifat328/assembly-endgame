0d7131d52049f739be893b07f69dd205531c7061
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2025-02-04 22:00:17 +0600

    Game Logic And Planning .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
 import { useState } from "react";
 import "./App.css";
+/*# What are the main containers of elements 
+    i need in this app?
+    => * need heading and sub headings , then game won and loss section based on
+    conditional rendering,
+    lebels as programming language , 8* input field for each charecter of word
+    * full set of charecter keybord section 
+    * Render new game button on when game won/loss 
+    *  
 
+  # What values will needed to be saved in state
+    vs what values can be derived from state?
+    => * randomly generated word can be stored on state,
+       * input char can be stored based on that we change field content 
+
+  #How will the user interact with the app? What
+ *   events do I need to handle?
+ *  => * user click and that char will apper on screen from 
+ * right to left 
+ * * if clicked char mached with stored word then 
+ * no change of color on input field char 
+ * if not it char will turned red and a programming language will be eleminated ,
+ * also that keybord button will turn red . other wise green if correct
+ *  => so we need keydown and clicked event  
+ * 
+ * */
 function App() {
   const [count, setCount] = useState(0);
 
