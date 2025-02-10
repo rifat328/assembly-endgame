683ab8f93411ea76eb5279727d322670ec096948
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2025-02-10 23:42:04 +0600

    keyboard done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Heading from "./component/Heading";
 import Status from "./component/Status";
 import Languages from "./component/Languages";
 import Inputs from "./component/Inputs";
+import { nanoid } from "nanoid";
 /*# What are the main containers of elements 
     i need in this app?
     => * need heading and sub headings , then game won and loss section based on
@@ -39,13 +40,31 @@ function App() {
       color={lang.color}
     />
   ));
+  const [currentWord, setCurrentWord] = useState("react");
+  const [gussedLetters, setGussedLetters] = useState([]);
+  console.log(gussedLetters);
+  const letterElements = currentWord
+    .split("")
+    .map((char) => <span key={nanoid()}>{char.toUpperCase()}</span>);
+
+  // keyboard
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const inputes = alphabet
+    .split("")
+    .map((char, index) => (
+      <Inputs key={index} gussedLetters={[""]} letter={char.toUpperCase()} />
+    ));
+
   return (
     <>
       <Heading />
       <Status />
       <div className="language-container">{language}</div>
-      <Inputs />
-      {<button>New Game</button>}
+      <section className="word">{letterElements}</section>
+      <section className="keyboard">{inputes}</section>
+      <section className="button-container">
+        {<button className="new-game">New Game</button>}
+      </section>
     </>
   );
 }
--- src/component/Inputs.jsx
+++ src/component/Inputs.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Inputs = () => {
-  return <div>Inputs</div>;
+const Inputs = (props) => {
+  // const gussedLetter = (letter) => {
+  //   setGussedLetters((letter) => [...gussedLetters].push(letter));
+  // };
+  return (
+    <button onClick={() => [...gussedLetters, props.letter]}>
+      {props.letter}
+    </button>
+  );
 };
 
 export default Inputs;
